8be11a6c6b7718367de325088e6e8d54e9452dc2
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2023-06-01 17:09:18 -0700

    functions and oop

--- class/js/function.js
+++ class/js/function.js
@@ -29,13 +29,29 @@ function printStudent() {
 // callstacks ===============
 // queue => first in first out
 // stack => last in first out
-console.log("program begins");
-console.log(studentA);
-printStudent();
-console.log("program ends");
+// console.log("program begins");
+// console.log(studentA);
+// printStudent();
+// console.log("program ends");
 
 // parallel & multi-thread => 2+ things to be processed/computed at the same time
 
 // concurrency != parallel => 1 thread => synchronously handling requests
 
 // callbacks
+
+// different ways of declaring functions
+function funcA() {}
+
+// normal anonymous function
+const myAnonymousFunc = function () {};
+
+// arrow functions (implied return keyword if written in the same line WITHOUT a function body {})
+const myArrowFunc = (a, b) => a + b;
+
+console.log(myArrowFunc(5, 7));
+
+[1, 2, 3].forEach((num, index, originalArray) => {
+	console.log(`number: ${num} - index: ${index}`);
+	console.log("originalArray", originalArray);
+});
