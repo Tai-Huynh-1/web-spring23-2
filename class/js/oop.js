7e812118c79950369380b3b10d579493fb4255c9
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2023-06-06 17:04:21 -0700

    inheritance and prototype chain

--- class/js/oop.js
+++ class/js/oop.js
@@ -203,3 +203,67 @@ const myBakery = new Store("85 degrees", "2023");
 console.log(myBakery.getNumberOfEmployees());
 myBakery.setNumberOfEmployees(100);
 console.log(myBakery.getNumberOfEmployees());
+
+// OOP Principle: Inheritance (achieve this in JS using prototypes)
+// (inheritance => "is a" relationship from child to parent)
+// Vehicle class is the parent to the SUV & Sedan class. Vehicle class is the super class
+class Vehicle {
+	constructor(brand, year, color) {
+		this.brand = brand;
+		this.year = year;
+		this.color = color;
+	}
+
+	useHorn() {
+		console.log("beep beep!");
+	}
+}
+
+class SUV extends Vehicle {
+	#type;
+	constructor(brand, year, color, doors) {
+		super(brand, year, color);
+
+		this.doors = doors;
+		this.#type = "suv";
+		this.seats = 7;
+	}
+
+	getSeats() {
+		return this.seats;
+	}
+
+	static getType() {
+		return this.#type;
+	}
+}
+
+class Sedan {
+	#type;
+	constructor(brand, year, color, doors) {
+		this.brand = brand;
+		this.year = year;
+		this.color = color;
+		this.doors = doors;
+
+		this.#type = "sedan";
+		this.seats = 5;
+	}
+
+	getSeats() {
+		return this.seats;
+	}
+
+	static getType() {
+		return this.#type;
+	}
+}
+
+console.log("my suv", new SUV("cadillac", "2023", "black", "4"));
+console.log("my sedan", new Sedan("mercedes", "2023", "white", "4"));
+
+// WHAT IS INHERITANCE & THE PROTOTYPE OBJECT?
+
+// COVER METHODS IN INHERITANCE
+
+// COVER JS DOCS
