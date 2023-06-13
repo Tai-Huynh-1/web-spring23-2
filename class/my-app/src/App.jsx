a555bb40086a79ecf6d0a624ea33e181e4c7d3e6
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2023-06-13 16:56:40 -0700

    jsx and props

--- class/my-app/src/App.jsx
+++ class/my-app/src/App.jsx
@@ -1,20 +1,23 @@
-import logo from "./logo.svg";
+import { Component } from "react";
 import "./App.css";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+// function App() {
+// 	const name = "Jake";
+// 	const element = <h1>Hello, {name}</h1>;
+
+// 	console.log("element", element);
+
+// 	return element;
+// }
+
+class App extends Component {
+	// constructor() {}
+
+	render() {
+		const name = "Jill";
+		const element = <h1>Hello, {name}</h1>;
+		return element;
+	}
 }
 
 export default App;
