64ad2c7e5e9cebd622134e9a0afe4e4d7b903a4f
Tai-Huynh-1 <93554992+Tai-Huynh-1@users.noreply.github.com>
2023-06-14 16:57:51 -0700

    react rendering - deriving class based component

--- class/my-app/src/App.jsx
+++ class/my-app/src/App.jsx
@@ -1,21 +1,13 @@
 import { Component } from "react";
 import "./App.css";
 
-// function App() {
-// 	const name = "Jake";
-// 	const element = <h1>Hello, {name}</h1>;
-
-// 	console.log("element", element);
-
-// 	return element;
-// }
-
 class App extends Component {
 	// constructor() {}
 
 	render() {
 		const name = "Jill";
 		const element = <h1>Hello, {name}</h1>;
+		console.log("element", element);
 		return element;
 	}
 }
